0ff243f3d07a838cbe04b74e8f60ec62581b314a
roubaka <lucas.martinez787@gmail.com>
2018-04-06 17:43:29 +0200

    working on login page

--- my-app/src/login.js
+++ my-app/src/login.js
@@ -30,9 +30,14 @@ class Login extends Component {
   }
 
   checkingDb = () => {
+    // database.ref() gets to db root
+    // once triggers event once on value change
+    // snap is a snapShot of the databse at this point
     database.ref('/users').once('value', snap => {
       snap.forEach(childSnapshot => {
+        // Checking 'username' value of the child of 'users'
         var usernameList = childSnapshot.child('/username').val();
+        // Checking for matchinv values into the db
         if(usernameList == this.state.username){
           this.setState({validName:false})
         } else
@@ -43,18 +48,21 @@ class Login extends Component {
     });
   }
 
+  // Clears input on first focus
   handleFocus = (e) => {
     if(this.state.focusedInput == false){
     e.target.value = ''
     }
   }
 
+  // Updates username value and checks if it already exists in db
   handleChange = (e) => {
     this.setState({username:e.target.value, validName:true, focusedInput:true})
     this.checkingDb()
   }
 
   handleClick = () => {
+    // If username doesn't exist in db
     if(this.state.validName){
       // Pushing new username into db - Store it into a variable
       var newUser = database.ref('/users').push({
@@ -62,21 +70,32 @@ class Login extends Component {
       });
       // Triggers login function inherited from Geoguide Component
       this.props.onClick(this.state.username,newUser.key);
+    // If username already exsists in db
     } else {
-      alert("Ce nom d'utilisateur existe déjà, est-ce bien toi?");
+      database.ref('/users').once('value', snap => {
+        snap.forEach(childSnapshot => {
+          var usernameList = childSnapshot.child('/username').val();
+          if (usernameList == this.state.username){
+            console.log(usernameList);
+            console.log(childSnapshot.key);
+          }
+        })
+      })
+      // alert("Ce nom d'utilisateur existe déjà, <br> merci de vérifier si c'est bien toi!");
     }
   }
 
   render(){
     // Condiditionnal rendering for user entry
     var validation;
-    var style;
+    var inputStyling;
     if(this.state.focusedInput && this.state.username != null){
-      if (this.state.validName == true)
-      validation = <span>Ce nom d'utilisateur est disponible</span>
-      ) : (
-        <span>Ce nom d'utilisateur existe déjà</span>
-      );
+      if (this.state.validName == true){
+        validation = <span>Ce nom d'utilisateur est disponible</span>
+      } else {
+        validation = <span>Ce nom d'utilisateur existe déjà,<br />êtes vous sûr que c'est bien vous?</span>
+        inputStyling = {backgroundColor:'red'};
+      }
     }
 
     return (
@@ -84,12 +103,12 @@ class Login extends Component {
         <h2>Bienvenue dans le Géoguide Lausanne</h2>
         <span>Avant de commencer il faut créer un nom d'utilisateur</span>
         <input type="text" required = "true" defaultValue = "Entrez votre nom"
-          style = {style}
+          style = {inputStyling}
           onChange = {this.handleChange} onFocus = {this.handleFocus}>
         </input>
         <button onClick = {this.handleClick}>
           Login
-        </button>
+        </button><br />
         {validation}
       </div>
     )
